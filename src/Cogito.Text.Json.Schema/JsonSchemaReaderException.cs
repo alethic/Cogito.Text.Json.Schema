f4b35a63fc83f4102d3f035210e1befa928ec3df
Jerome Haltom <wasabi@cogito.cx>
2019-12-28 18:04:51 -0600

    Fix comments.

--- src/Cogito.Text.Json.Schema/JsonSchemaReaderException.cs
+++ src/Cogito.Text.Json.Schema/JsonSchemaReaderException.cs
@@ -1,25 +1,43 @@
 ﻿using System;
-using System.Runtime.Serialization;
 
 namespace Cogito.Text.Json.Schema
 {
-    [Serializable]
-    internal class JsonSchemaReaderException : Exception
+
+    /// <summary>
+    /// Describes an exception that occurred while reading JSON schema.
+    /// </summary>
+    public class JsonSchemaReaderException : Exception
     {
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
         public JsonSchemaReaderException()
         {
+
         }
 
-        public JsonSchemaReaderException(string message) : base(message)
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="message"></param>
+        public JsonSchemaReaderException(string message) :
+            base(message)
         {
+
         }
 
-        public JsonSchemaReaderException(string message, Exception innerException) : base(message, innerException)
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
+        public JsonSchemaReaderException(string message, Exception innerException) :
+            base(message, innerException)
         {
+
         }
 
-        protected JsonSchemaReaderException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-        }
     }
+
 }
